23768cba72e616173eb09e5cc0c9f11808b664d0
Ksav <120297504+Ksav4514@users.noreply.github.com>
2022-12-11 15:46:06 -0500

    Update streamlit_app.py
    changed header and text from title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Breakfast menu')
-streamlit.title('Omelette, Pancake, French Toast')
+streamlit.header('Breakfast menu')
+streamlit.text('Omelette, Pancake, French Toast')
